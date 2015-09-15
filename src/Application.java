548aab97749da4cddfe28c59fa7639c5aa4bcd9e
Bobfromtheparty <kjr9@pct.edu>
2015-09-15 15:41:49 -0400

    Added lines to the code

--- src/Application.java
+++ src/Application.java
@@ -5,5 +5,10 @@ public class Application {
 
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println(5 + 5);
+
+
+
+        System.out .println("done");
     }
 }
